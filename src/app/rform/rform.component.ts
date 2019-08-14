32f0508c020316258fa384dc45534af99769558f
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-14 14:39:25 +0530

    reactive form created - ui done

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -17,6 +17,22 @@ export class RformComponent implements OnInit {
       title: new FormControl('default Title', [
         Validators.required,
         Validators.minLength(5)
+      ]),
+      description: new FormControl('default description', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      severity: new FormControl('0', [Validators.required]),
+      isResolved: new FormControl(false, [Validators.required]),
+      user: new FormControl('default user', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      createdAt: new FormControl(new Date(), [Validators.required]),
+      id: new FormControl(-1, [Validators.required]),
+      comments: new FormControl('default comments', [
+        Validators.required,
+        Validators.minLength(5)
       ])
     });
   }
